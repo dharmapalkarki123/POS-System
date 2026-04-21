c77e83d56a02d9a7c17d1a977c6a25f43ed35784
dharmapalkarki123 <dharmapalkarki@gmail.com>
2026-04-21 19:40:43 +0545

    commit

---
+++ src/main/java/com/pos/configuration/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.pos.configuration;
+
+public class JwtConstant {
+
+    public static final String JWT_SECRET = "safsfsfsadasdsfdfesefeawgehdf,dgre,gergdfeg,gre,fr,egee,gregeryerggtrteergsfg,hsdhr";
+    public static final String JWT_HEADER = "Authentication";
+
+}
