424c72ac3c5e0848ac992d14a2071a680b711dcb
dharmapalkarki123 <dharmapalkarki@gmail.com>
2026-05-11 21:36:12 +0545

    commit

---
+++ src/main/java/com/pos/controller/StoreController.java
@@ -0,0 +1,105 @@
+package com.pos.controller;
+
+import com.pos.exception.UserException;
+import com.pos.modal.Store;
+import com.pos.modal.Users;
+import com.pos.payload.dto.storeDto;
+import com.pos.service.StoreService;
+import com.pos.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/store")
+public class StoreController {
+
+    private StoreService storeService;
+    private UserService userService;
+
+    public StoreController(StoreService storeService, UserService userService) {
+        this.storeService = storeService;
+        this.userService = userService;
+    }
+
+
+
+    @PostMapping
+    public ResponseEntity<storeDto> createStore(@RequestBody storeDto StoreDto,
+                                                @RequestHeader("Authorization")String jwt) throws UserException {
+
+        Users user=userService.getUserFromJwtToken(jwt);
+
+        return ResponseEntity.ok(storeService.createStore(StoreDto,user));
+
+
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<storeDto> getStoreById(@PathVariable Long id,
+                                                @RequestHeader("Authorization")String jwt) throws Exception {
+
+        Users user=userService.getUserFromJwtToken(jwt);
+
+        return ResponseEntity.ok(storeService.getStoreById(id));
+
+
+    }
+
+
+
+    @GetMapping
+    public ResponseEntity<List<storeDto>> createStore(
+                                              @RequestHeader("Authorization")String jwt) throws UserException {
+
+
+
+        return ResponseEntity.ok(storeService.getAllStore());
+
+
+    }
+
+
+
+
+    @PostMapping("/admin")
+    public ResponseEntity<Store> getStoreByAdmin(
+            @RequestHeader("Authorization")String jwt) throws UserException {
+
+
+
+        return ResponseEntity.ok(storeService.getStoreByAdmin());
+
+
+    }
+
+
+    @GetMapping("/employee")
+    public ResponseEntity<storeDto> getStoreByEmployee(
+            @RequestHeader("Authorization")String jwt) throws UserException {
+
+
+
+        return ResponseEntity.ok(storeService.getStoreByEmployee());
+
+
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<storeDto> updateStore(@PathVariable Long id,
+            @RequestBody storeDto StoreDto) throws Exception {
+
+
+
+        return ResponseEntity.ok(storeService.updateStore(id, StoreDto);
+
+
+    }
+
+
+
+
+}
